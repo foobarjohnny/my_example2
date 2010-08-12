52cba279cce6853b64e27a8980f9808f50c28a0f
guoxin77 <guoxin77@7e0b5509-6f4a-8bf9-4c4a-278d71e247f9>
2010-08-12 03:58:24 +0000

    删除了所有的classes中的java源代码

--- auction/src/org/auction/module/admin/commodity/service/AuctionService.java
+++
@@ -1,8 +0,0 @@
-package org.auction.module.admin.commodity.service;
-
-import org.auction.module.admin.commodity.data.AuctionData;
-
-public interface AuctionService {
-
-	public void auction(AuctionData data);
-}
--- auction/src/org/auction/module/view/show/service/IShowService.java
+++
@@ -1,8 +0,0 @@
-package org.auction.module.view.show.service;
-
-import org.auction.module.view.show.data.ShowData;
-
-public interface IShowService {
-
-	public void show(ShowData model) throws Exception;
-}
