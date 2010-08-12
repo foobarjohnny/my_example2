f5596ad41bd35e0e6c38e96b2b2cdab5438b19a6
guoxin77 <guoxin77@7e0b5509-6f4a-8bf9-4c4a-278d71e247f9>
2010-08-12 03:58:24 +0000

    删除了所有的classes中的java源代码

--- trunk/auction/src/org/auction/module/admin/user/service/UserAccountService.java
+++
@@ -1,9 +0,0 @@
-package org.auction.module.admin.user.service;
-
-import org.auction.module.admin.user.data.UserAccountData;
-import org.mobile.common.exception.GeneralException;
-
-public interface UserAccountService {
-
-	public void account(UserAccountData model) throws GeneralException;
-}
--- trunk/auction/src/org/auction/module/admin/view/service/ViewCommodityService.java
+++
@@ -1,9 +0,0 @@
-package org.auction.module.admin.view.service;
-
-import org.auction.module.admin.view.data.ViewData;
-import org.mobile.common.exception.GeneralException;
-
-public interface ViewCommodityService {
-
-	public void show(ViewData model) throws GeneralException;
-}
